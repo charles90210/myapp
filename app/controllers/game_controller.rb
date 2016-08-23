835a482def7f40122db4dad11769d8f1ce0a1c08
charles and leean <charles.ukpai@yahoo.com>
2016-08-23 15:57:58 -0700

    completedHiLo

--- app/controllers/game_controller.rb
+++ app/controllers/game_controller.rb
@@ -6,11 +6,11 @@ class GameController < ApplicationController
     #create a variable name to collect the name entered in the form
     @name = params[:name]
     #set a cookie to hold the users name
-    cookies[:name] = @name
+    session[:name] = @name
     #creates a variable cookie with a secret random number, sets this as a cookie in the broswer and allows you to have the random number as the same until the game starts over.
-    cookies[:secret_number] = Random.rand(1 .. 50)
+    session[:secret_number] = Random.rand(1 .. 50)
     #creates a varible to count the number of tries and set it to zero
-    cookies[:num_tries] = 0
+    session[:num_tries] = 0
     #put number of tries in cookie
 
     render "try.html.erb"
@@ -22,30 +22,30 @@ class GameController < ApplicationController
   def try
 
     #get the name cookie
-    @name = cookies[:name]
+    @name = session[:name]
     #get the secret number cookie
-    @secret_number = cookies[:secret_number]
+    @secret_number = session[:secret_number]
     #creates a variable for number of tries and assigns the cookie value to it
-    @num_tries = cookies[:num_tries].to_i
+    @num_tries = session[:num_tries].to_i
     #increments the number of tries by one
     @num_tries = @num_tries + 1
     #resets the value of the cookie to the value of the num_tries variable
-    cookies[:num_tries] = @num_tries
+    session[:num_tries] = @num_tries
     #create a variable for the users guess
     user_guess = params[:guess].to_i
     #creates a vairable for the result outcome
     @result = ""
     #writes an if statement checking for the number of tries and dispalying a statement for each outcome
 
-    if user_guess > cookies[:secret_number].to_i
+    if user_guess > session[:secret_number].to_i
       @result = "number too high, try again"
       render "result.html.erb"
       #elsif statement compares the number of tries and increments it by one
-    elsif user_guess < cookies[:secret_number].to_i
+    elsif user_guess < session[:secret_number].to_i
       @result =  "number too low, try again"
       render "result.html.erb"
       #else statement that displays a statement when the user guesses the secret number correctly
-    else user_guess == cookies[:secret_number].to_i
+    else user_guess == session[:secret_number].to_i
       @result =  "Congratulations you win!!"
       render "win.html.erb"
     end
